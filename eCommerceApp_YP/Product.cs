72420c8b155ee1311c78d3687e8791120f647d65
yashvipatel-yp <yashvip342@gmail.com>
2024-06-14 02:40:02 -0400

    increase & decrease stock func added

--- eCommerceApp_YP/Product.cs
+++ eCommerceApp_YP/Product.cs
@@ -46,5 +46,18 @@ namespace eCommerceApp_YP
                 throw new ArgumentOutOfRangeException(nameof(stock), "Stock level must be between 1 and 100000.");
         }
 
+        public void IncreaseStock(int unit)
+        {
+            Stock = (unit > 0) ? Stock + unit : throw new ArgumentOutOfRangeException(nameof(unit), "Stock to increase must be positive.");
+        }
+
+        public void DecreaseStock(int unit)
+        {
+            if (unit <= 0)
+                throw new ArgumentOutOfRangeException(nameof(unit), "Stock to decrease must be positive.");
+
+            Stock = (unit <= Stock) ? Stock - unit : throw new InvalidOperationException("Insufficient stock to complete the operation.");
+        }
+
     }
 }
--- eCommerceApp_YP/Program.cs
+++ eCommerceApp_YP/Program.cs
@@ -12,6 +12,19 @@ namespace eCommerceApp_YP
 
                 ProductDetails(product);
 
+                // Increase stock
+                product.IncreaseStock(5);
+                Console.WriteLine("\n Increasing 5 units.");
+                ProductDetails(product);
+
+                // Decrease stock
+                product.DecreaseStock(2);
+                Console.WriteLine("\n Decreasing 2 units.");
+                ProductDetails(product);
+
+                // Attempt to decrease stock beyond available amount
+                Console.WriteLine("\nAttempting to decrease stock by 150 units...");
+                product.DecreaseStock(150);
             }
             catch (Exception exception) when (exception is ArgumentOutOfRangeException || exception is InvalidOperationException)
             {
