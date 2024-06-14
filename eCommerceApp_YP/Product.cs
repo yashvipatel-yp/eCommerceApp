b7c8735df8a57910e23bece4062badcc6cfa9b6b
yashvipatel-yp <yashvip342@gmail.com>
2024-06-14 02:34:01 -0400

    functionality added

---
+++ eCommerceApp_YP/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace eCommerceApp_YP
+{
+    public class Product
+    {
+        public int ProductID { get; private set; }
+        public string ProductName { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ValidateProductID(productId);
+            ValidateProductName(productName);
+            ValidatePrice(price);
+            ValidateStock(stock);
+
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        private void ValidateProductID(int productId)
+        {
+            if (productId < 1 || productId > 10000)
+                throw new ArgumentOutOfRangeException(nameof(productId), "ProductID must be in the range 1 to 10000.");
+        }
+
+        private void ValidateProductName(string productName)
+        {
+            if (string.IsNullOrWhiteSpace(productName))
+                throw new ArgumentNullException(nameof(productName), "Product name cannot be empty or null.");
+        }
+
+        private void ValidatePrice(decimal price)
+        {
+            if (price < 1 || price > 5000)
+                throw new ArgumentOutOfRangeException(nameof(price), "Price must be within the range of 1 to 5000.");
+        }
+
+        private void ValidateStock(int stock)
+        {
+            if (stock < 1 || stock > 100000)
+                throw new ArgumentOutOfRangeException(nameof(stock), "Stock level must be between 1 and 100000.");
+        }
+
+    }
+}
--- eCommerceApp_YP/Program.cs
+++ eCommerceApp_YP/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace eCommerceApp_YP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var product = new Product(1, "Smartphone", 999.49m, 100);
+
+                ProductDetails(product);
+
+            }
+            catch (Exception exception) when (exception is ArgumentOutOfRangeException || exception is InvalidOperationException)
+            {
+                Console.WriteLine($"Error: {exception.Message}");
+            }
+        }
+
+        private static void ProductDetails(Product product)
+        {
+            Console.WriteLine($"Product ID: {product.ProductID}, Product: {product.ProductName}, Price: {product.Price:C}, Current Stock: {product.Stock}");
+        }
+    }
+}
