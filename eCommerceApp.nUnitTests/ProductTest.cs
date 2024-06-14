86c7e20f0779efce26921124260f417894d29c1a
yashvipatel-yp <yashvip342@gmail.com>
2024-06-14 12:26:13 -0400

    price and stock test cases added

--- eCommerceApp.nUnitTests/ProductTest.cs
+++ eCommerceApp.nUnitTests/ProductTest.cs
@@ -96,5 +96,78 @@ namespace eCommerceApp.nUnitTests
             Assert.That(() => new Product(productId, productName, price, stock), Throws.TypeOf<ArgumentNullException>());
         }
 
+        // Price tests
+        [Test]
+        public void Price_Valid_ShouldCreate()
+        {
+            // Arrange
+            decimal price = 100;
+
+            // Act
+            var product = new Product(1, "Smartphone", price, 10);
+
+            // Assert
+            Assert.That(product.Price, Is.EqualTo(price));
+        }
+
+        [Test]
+        public void Price_OutOfRange_ShouldThrow()
+        {
+            // Arrange
+            decimal price = 5001;
+
+            // Act & Assert
+            Assert.That(() => new Product(1, "Smartphone", price, 10), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Price_Max_ShouldCreate()
+        {
+            // Arrange
+            decimal price = 5000;
+
+            // Act
+            var product = new Product(1, "Smartphone", price, 10);
+
+            // Assert
+            Assert.That(product.Price, Is.EqualTo(price));
+        }
+
+        // Stock tests
+        [Test]
+        public void Stock_Valid_ShouldCreate()
+        {
+            // Arrange
+            int stock = 10;
+
+            // Act
+            var product = new Product(1, "Smartphone", 100, stock);
+
+            // Assert
+            Assert.That(product.Stock, Is.EqualTo(stock));
+        }
+
+        [Test]
+        public void Stock_OutOfRange_ShouldThrow()
+        {
+            // Arrange
+            int stock = 100001;
+
+            // Act & Assert
+            Assert.That(() => new Product(1, "Smartphone", 100, stock), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Stock_Max_ShouldCreate()
+        {
+            // Arrange
+            int stock = 100000;
+
+            // Act
+            var product = new Product(1, "Smartphone", 100, stock);
+
+            // Assert
+            Assert.That(product.Stock, Is.EqualTo(stock));
+        }
     }
 }
